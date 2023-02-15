8ccf76a930b27689d4324dacd6abf9b745c21b16
edmondac-admin <a.c.edmondson@bham.ac.uk>
2023-02-15 15:18:27 +0000

    Use decorator to register admin config

--- django_scheduled_tasks/admin.py
+++ django_scheduled_tasks/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from .models import ScheduledTask
 
 
+@admin.register(ScheduledTask)
 class ScheduledTaskAdmin(admin.ModelAdmin):
     readonly_fields = ('last_timestamp', 'last_success')
-
-
-admin.site.register(ScheduledTask, ScheduledTaskAdmin)
