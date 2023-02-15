273f130f290ea451a2b0525bf68c445966da8dd5
edmondac-admin <a.c.edmondson@bham.ac.uk>
2023-02-15 12:25:14 +0000

    flake8

--- django_scheduled_tasks/models.py
+++ django_scheduled_tasks/models.py
@@ -10,7 +10,7 @@ class ScheduledTask(models.Model):
     """
     func = models.CharField(max_length=100, unique=True)  # importable function name
     interval_minutes = models.IntegerField(validators=[MinValueValidator(1),
-                                                       MaxValueValidator(1440)]) # 1 min to 24 hours
+                                                       MaxValueValidator(1440)])  # 1 min to 24 hours
     enabled = models.BooleanField(default=True)
     last_timestamp = models.DateTimeField(null=True, blank=True)
     last_success = models.BooleanField(null=True, blank=True)
