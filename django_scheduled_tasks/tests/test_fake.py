5dcbc21cf1ebe348d4efb8c74df66608fe79148d
Leo Turnell-Ritson (Advanced Research Computing) <l.n.turnell-ritson@bham.ac.uk>
2024-07-19 17:42:41 +0100

    flake8

--- django_scheduled_tasks/tests/test_fake.py
+++ django_scheduled_tasks/tests/test_fake.py
@@ -1,7 +1,10 @@
+"""Example test case."""
 from django.test import TestCase
 
 
 class FakeTestCase(TestCase):
+    """Example test case."""
 
     def test_fake(self):
+        """Test method to validate a 'fake' assertion."""
         self.assertEqual(1, 1)
--- run_tests.py
+++ run_tests.py
@@ -1,28 +1,32 @@
 #!/usr/bin/env python
+"""A module for running Django tests for the django_scheduled_tasks app.
+
+Module Usage:
+    $ python run_tests.py
+"""
 import sys
 import os
 
 import django
 from django.conf import settings
 from django.test.utils import get_runner
-from django.core.management import call_command
 
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 APP_DIR = os.path.join(BASE_DIR, "django_scheduled_tasks")
-TEST_DIR = os.path.join(APP_DIR, "tests")
+
 
 if __name__ == "__main__":
     settings.configure(
         DEBUG=True,
-        USE_TZ = True,
-        DATABASES = {
+        USE_TZ=True,
+        DATABASES={
             "default": {
                 "NAME": os.path.join(APP_DIR, 'django_scheduled_tasks.sqlite3'),
                 "ENGINE": "django.db.backends.sqlite3",
             }
         },
-        INSTALLED_APPS = [
+        INSTALLED_APPS=[
             "django_scheduled_tasks",
         ],
         NOSE_ARGS=["-s"],
