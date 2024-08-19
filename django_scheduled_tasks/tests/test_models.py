08e0f3c1b7c5fc7d1963aea8181751a3c4093d20
j43333 <141174254+j43333@users.noreply.github.com>
2024-08-19 16:27:23 +0100

    tests added for scheduled tasks

--- django_scheduled_tasks/tests/test_models.py
+++ django_scheduled_tasks/tests/test_models.py
@@ -1,10 +1,37 @@
 """Example test case."""
 from django.test import TestCase
+from django_scheduled_tasks.models import ScheduledTask
+
+IS_EXECUTED = False 
+
+def test_func():
+    """if the test is correctly executed, it will then pass as True """  
+    IS_EXECUTED = True 
 
 
-class FakeTestCase(TestCase):
-    """Example test case."""
+class ScheduledTaskCase(TestCase):
+    """Example test case, testing the three functions (execute, str, save) in the Models module"""
 
-    def test_fake(self):
-        """Test method to validate a 'fake' assertion."""
-        self.assertEqual(1, 1)
+    @classmethod
+    def setUpTestData(cls):
+        """ example object being used in each test  """  
+        cls.t1 = ScheduledTask.objects.create(
+            func='django_scheduled_tasks.tests.test_models.test_func',
+            interval_minutes=10
+        )
+
+    def test_str(self):
+        """ inserting the test obj into the answer string and seeing if it equals the same as the real result """  
+        self.assertEqual(
+            str(self.t1),
+            f"Run {self.t1.func} every {self.t1.interval_minutes} minutes ({'enabled' if self.t1.enabled else 'disabled'})"
+        )
+
+    def test_execute(self): 
+        """ runs the execute function and changes IS_EXECUTED to True when ran correctly """  
+
+        self.assertFalse(IS_EXECUTED)
+        self.t1.execute()
+        self.assertTrue(IS_EXECUTED)
+
+    
