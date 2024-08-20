67dff80eae4cc206d3d180c0ef43d55bfea57f49
j43333 <141174254+j43333@users.noreply.github.com>
2024-08-20 09:52:33 +0100

    fixing flex

--- django_scheduled_tasks/tests/test_models.py
+++ django_scheduled_tasks/tests/test_models.py
@@ -5,7 +5,7 @@ from unittest.mock import patch
 
 
 def test_func():
-    """If the test is correctly executed, it will then pass as True."""  
+    """If the test is correctly executed, it will then pass as True."""
     pass
 
 
@@ -14,27 +14,22 @@ class ScheduledTaskCase(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        """Example object being used in each test."""  
+        """Example object being used in each test."""
         cls.t1 = ScheduledTask.objects.create(
             func='django_scheduled_tasks.tests.test_models.test_func',
             interval_minutes=10
         )
 
     def test_str(self):
-        """Inserting the test obj into the answer string and seeing if it equals the same as the real result."""  
+        """Inserting the test obj into the answer string and seeing if it equals the same as the real result."""
         self.assertEqual(
             str(self.t1),
             f"Run {self.t1.func} every {self.t1.interval_minutes} minutes ({'enabled' if self.t1.enabled else 'disabled'})"
         )
 
     @patch('django_scheduled_tasks.tests.test_models.test_func')
-    def test_execute(self, mock_test_func): 
-        """Runs the execute function and changes IS_EXECUTED to True when ran correctly."""  
+    def test_execute(self, mock_test_func):
+        """Runs the execute function and changes IS_EXECUTED to True when ran correctly."""
         self.t1.execute()
         mock_test_func.assert_called_once()
-
-
-
-        # see if the global var is called self.t1.execute and see if func has been called 
-
     
