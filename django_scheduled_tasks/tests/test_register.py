d0b73896dcc9b0391207b8bfd3ff7f4e2d21f690
Leo Turnell-Ritson (Advanced Research Computing) <l.n.turnell-ritson@bham.ac.uk>
2024-08-23 14:04:50 +0100

    consolodated tests

--- django_scheduled_tasks/tests/test_register.py
+++ django_scheduled_tasks/tests/test_register.py
@@ -37,3 +37,23 @@ class RegisterTaskTest(TestCase):
             pass
 
         self.assertFalse(ScheduledTask.objects.all().exists())
+
+
+class SpecificScheduledTaskTest(TestCase):
+    """Testing the schedule_task function."""
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.func = 'django_scheduled_tasks.tests.test_models.test_func'
+
+    @patch('django_scheduled_tasks.models.ScheduledTask.objects.create')
+    def test_schedule_task_with_invalid_day(self, mock_create):
+        with self.assertRaises(ValueError):
+            schedule_task(day=-1, hour=12)
+
+    def test_schedule_task_correct(self):
+        if day is not None and (day < 0 or day > 6):
+            raise ValueError("Day must be between 0 (Monday) and 6 (Sunday)")
+
+        if hour is not None and (hour < 0 or hour > 23):
+            raise ValueError("Hour must be between 0 and 23")
